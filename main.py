68e7db38fd20b71702a6d505ed431b0047e4f12c
haodong1234 <donghao0212@gmail.com>
2021-04-01 13:40:54 +0200

    conflikt commit

--- main.py
+++ main.py
@@ -6,12 +6,14 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 def print_hallo():
     print('Hello world!!!')
 # Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
 
